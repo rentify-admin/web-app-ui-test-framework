50860e695a33d1d6f00d8b771061a9d52a66b365
Dhaval <dhaval.thankral@verifast.com>
2025-10-09 18:55:46 +0530

    UPDATE: Address menu heartbeat test added
    Ticket: QA-155

---
+++ tests/heartbeat_addresses_menus.spec.js
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+import loginForm from './utils/login-form';
+import { admin } from './test_config';
+
+
+test.describe('heartbeat-address-menus.spec', () => {
+
+    test('Should check Address heartbeat', {
+        tag: ['@core', '@smoke', '@regression'],
+    }, async ({ page }) => {
+
+        await page.goto('/');
+        await loginForm.fill(page, admin);
+        await loginForm.submit(page);
+        await expect(page.getByTestId('household-status-alert')).toBeVisible({ timeout: 10_000 });
+
+        const addressMenus = await page.getByTestId('address-menu');
+
+        await addressMenus.click()
+
+    })
+})
