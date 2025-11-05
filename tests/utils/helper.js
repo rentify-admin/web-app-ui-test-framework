07481a367e9625eace2608d7e6f122aec2707af1
Dhaval <dhaval.thankral@verifast.com>
2025-11-05 21:16:00 +0530

    UPDATE: Helper function updated

--- tests/utils/helper.js
+++ tests/utils/helper.js
@@ -70,6 +70,14 @@ const kebabToTitleCase = (str) => {
         .join(' ');
 }
 
+const generateUUID = () => {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+};
+
 export {
     joinUrl,
     escapeRegex,
@@ -79,5 +87,6 @@ export {
     getAmount,
     getSafeAmount,
     getCentsToDollarsSafe,
-    kebabToTitleCase
+    kebabToTitleCase,
+    generateUUID
 };
