21f9bd2a37ed17a79368d854f9b9fbb4970c286e
Ignacio Secco <ignacio.martinez@verifast.com>
2025-12-03 16:59:42 -0300

    test: Add affordable_occupant tag

--- tests/applicant_type_workflow_affordable_occupant.spec.js
+++ tests/applicant_type_workflow_affordable_occupant.spec.js
@@ -12,7 +12,7 @@ let allTestsPassed = true;
 
 test.describe('applicant_type_workflow_affordable_occupant', () => {
     test('Should complete applicant flow with affordable occupant applicant type', { 
-        tag: ['@core', '@regression', '@staging-ready', '@rc-ready'],
+        tag: ['@core', '@regression', '@staging-ready', '@rc-ready', '@affordable_occupant'],
     }, async ({ page }) => {
         test.setTimeout(450000);
         
